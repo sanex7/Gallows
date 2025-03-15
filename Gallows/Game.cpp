083738b83305dc8b193e421be8dcdb429d9aefff
sanex7 <159659818+sanex7@users.noreply.github.com>
2025-03-15 11:47:30 +0200

    Update Game.cpp

--- Gallows/Game.cpp
+++ Gallows/Game.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "ConsoleGameView.h"
+#include "WordsManager.h"
 #include "Game.h"
+
+int main()
+{
+    WordsManager manager;
+    ConsoleGameView consoleView;
+    Game game(std::make_shared<WordsManager>(manager), std::make_unique<ConsoleGameView>(consoleView));
+    game.Start();
+
+    return 0;
+}
